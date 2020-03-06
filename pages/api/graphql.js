3a9891130590218b224677396f7239f403a8c0b3
Yannick <panis@roller.de>
2020-03-06 17:39:40 +0100

    move schema into seperate file, remove apollo-boost, refactor apollo client

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,42 +1,14 @@
-import { ApolloServer, gql } from 'apollo-server-micro';
-import { mergeResolvers, mergeTypeDefs } from "graphql-toolkit";
+import { ApolloServer } from 'apollo-server-micro';
 import connectDB from '../../lib/mongoose';
-import { brandResolvers } from "../../src/api/brand/resolvers";
-import { brandMutations } from "../../src/api/brand/mutations";
-import Brand from '../../src/api/brand/Brand.graphql';
+import { schema } from "../../lib/schema";
 
-const fakeTypeDefs = gql`
-    type Query {
-        sayHello: String
-    }
-`
-
-const fakeResolvers = {
-    Query: {
-        sayHello: () => 'Hello :)'
-    }
-};
-
-const resolvers = mergeResolvers([
-    fakeResolvers,
-    brandResolvers,
-    brandMutations
-]);
-
-const typeDefs = mergeTypeDefs([
-    fakeTypeDefs,
-    Brand
-]);
-
-const apolloServer = new ApolloServer({ typeDefs, resolvers })
+const apolloServer = new ApolloServer({ schema })
 
 export const config = {
     api: {
         bodyParser: false
     }
 };
-
-
 
 const server = apolloServer.createHandler({ path: '/api/graphql'});
 
